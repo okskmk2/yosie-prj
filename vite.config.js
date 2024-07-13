a83f9ae2221f7526b953963976892660a977ebfe
okskmk2 <okskmk2@naver.com>
2024-07-13 23:34:54 +0700

    t

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/docs',
+  base: process.env.NODE_ENV === "production" ? "/climate-tft/" : "/",
   build: {
     outDir: 'docs'
   },
